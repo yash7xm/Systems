8423cb893c9a0cbcb00fb772add33e000243e277
yash7xm <yash.7xm@gmail.com>
2024-03-01 18:47:30 +0530

    message convey

--- src/realtime-communication/socket-io.js
+++ src/realtime-communication/socket-io.js
@@ -16,6 +16,9 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
 });
 
 server.listen(3000, () => {
