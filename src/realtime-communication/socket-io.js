91c7c79e1586602571d9936860073b469a6218d5
yash7xm <yash.7xm@gmail.com>
2024-03-01 18:42:11 +0530

    socket io connection established

--- src/realtime-communication/socket-io.js
+++ src/realtime-communication/socket-io.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
